2f544dfb95ae4f4750a7fb85eb9c9cc31d978088
cc <cc@ccdeiMac.lan>
2018-02-11 19:44:26 +0800

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -7,9 +7,8 @@ var imagemin = require('gulp-imagemin');
 var del = require('del');
 var babel = require('gulp-babel');
 var cleanCSS = require('gulp-clean-css');
-var concat = require('gulp-concat'); //- 多个文件合并为一个；
-var rev = require('gulp-rev'); //- 对文件名加MD5后缀
-var revCollector = require('gulp-rev-collector'); //- 路径替换
+var watch = require('gulp-watch');
+var fileinclude = require('gulp-file-include');
 
 gulp.task('html', function() {
   var options = {
@@ -22,30 +21,33 @@ gulp.task('html', function() {
     minifyJS: true, //压缩页面JS
     minifyCSS: true //压缩页面CSS
   };
-  gulp.src('./*.html')
-    .pipe(htmlmin(options))
+  gulp.src(['index.html', 'news.html', 'aboutus.html'])
+    .pipe(fileinclude({
+      prefix: '@@',
+      basepath: '@file'
+    }))
+    // .pipe(htmlmin(options))
     .pipe(gulp.dest('dist'));
 });
-
 gulp.task('css', function() {
   var plugins = [
     autoprefixer({ browsers: ['last 2 version', 'ie>= 10'] }),
   ];
   return gulp.src('./css/*.css')
     .pipe(postcss(plugins))
-    .pipe(cleanCSS())
+    // .pipe(cleanCSS())
     .pipe(gulp.dest('./dist/css'))
 });
 
 gulp.task('js', () => {
   gulp.src('./js/*.js')
     .pipe(babel())
-    .pipe(uglify())
+    // .pipe(uglify())
     .pipe(gulp.dest('dist/js'));
 });
 gulp.task('img', () =>
   gulp.src('./img/*')
-  .pipe(imagemin())
+  // .pipe(imagemin())
   .pipe(gulp.dest('dist/img'))
 );
 gulp.task('copyfont', function() {
@@ -57,5 +59,10 @@ gulp.task('clean', function(cb) {
     'dist/*'
   ], cb);
 });
-
+gulp.task('watch', function() {
+  gulp.watch('./*.html', ['html']);
+  gulp.watch('./css/*.css', ['css']);
+  gulp.watch('./img/*', ['img']);
+  gulp.watch('./js/*.js', ['js']);
+})
 gulp.task('default', ['clean', 'css', 'js', 'html', 'img', 'copyfont']); //定义默认任务 elseTask为其他任务，该示例没有定义elseTask任务
